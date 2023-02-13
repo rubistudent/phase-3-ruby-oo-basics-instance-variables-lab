7dd9676bf1c1065081a5b55f9cdd9390a2dac18f
www.rubiluqman7@gmail.com <www.rubiluqman7@gmail.com>
2023-02-13 04:58:01 +0300

    completed

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,14 @@
-class Dog
+class Do
+ def name = (dog_name)
+   @this_dogs_name = dog_name
 
 end
+
+def name
+     @this_dogs_name
+end
+end
+lassie + Dog .new
+lassie.name = "lassle"
+
+puts lassie.name
