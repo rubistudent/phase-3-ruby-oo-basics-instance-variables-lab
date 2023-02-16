c9e997d0f78303dcc2d58c9c6bb9158000732b2f
www.rubiluqman7@gmail.com <www.rubiluqman7@gmail.com>
2023-02-16 21:01:05 +0300

    completed

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,15 @@
-class Do
- def name = (dog_name)
-   @this_dogs_name = dog_name
 
-end
-
-def name
-     @this_dogs_name
-end
-end
-lassie + Dog .new
-lassie.name = "lassle"
-
-puts lassie.name
+class Dog
+     def name=(dogs_name)
+       @this_dogs_name = dogs_name
+     end
+   
+     def name
+       @this_dogs_name
+     end
+   end
+   
+   lassie = Dog.new
+   lassie.name = "Lassie"
+   
+   puts lassie.name
